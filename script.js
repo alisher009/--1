8fb8b0ab91ec1fbdb81b294036bc59e37f72a7aa
alisher <chexramchik@gmail.com>
2022-03-31 19:06:49 +0500

    перваяя

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// <--------1------->
+
+
+
+
+// let name = prompt('Как вас Зовут')
+
+// let newName = {
+
+//     name: name,
+// }
+
+// alert(`Здравствуйтe ${name} ваш id ${Random(8)}`)
+
+// function Random( id){
+//     const number = "79868678327838vbdjsqiewjmochjasoasc90ewjkcicxkjcxbo";
+
+//     let randomNumber = "";
+
+//     for (let i=0; i<id; i++){
+//         let index = Math.floor(Math.random()* number.length);
+//         randomNumber +=number[index];
+//     }
+
+//     return randomNumber;
+//     }
+
+//     console.log( Random(8));
+
+
+
+// <------2----->
+
+
+
+
+function newСompany( value ,base){
+    value= +prompt( 'сколько компов ')
+    base = +prompt(' cколько брак')
+    return value / base * 100
+}
+console.log(newСompany())
+
+
+
+// <------3------> 
+
+// function newName(arg){
+//         const a = prompt('Как вас зовут')
+//         let Random = ''
+//         for(let i = 0; i < arg; i++){
+//             let randomName = Math.floor(Math.random( ) * a ) ;
+//            Random  = a
+//         }
+//         return Random
+//     }
+// alert( `Здравствуйтe  вот ваш gmail ${newName(4) +'@gmail.com'}`)
+
+
+//     console.log(newName(4) +'@gmail.com');
